d4832b31c57a770d3b2e553c6fbd63d2bf681eef
link384 <ol5848852@gmail.com>
2025-07-15 02:50:23 -0400

    Configurar la tienda para generar APK de Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yeroshop.app',
+  appName: 'Yero Shop',
+  webDir: 'build'
+};
+
+export default config;
